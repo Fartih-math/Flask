87ad97c73b3cf284d3d082641f221006a3841625
Fartih-math <farisfatihmtk@gmail.com>
2026-06-10 16:30:23 +0700

    Update encryption_utils.py

--- encryption_utils.py
+++ encryption_utils.py
@@ -1,23 +1,18 @@
 from cryptography.fernet import Fernet
 import os
 
-KEY_FILE = 'secret.key'
+KEY_ENV = 'ENCRYPTION_KEY'
 
 def load_or_generate_key():
-    if os.path.exists(KEY_FILE):
-        with open(KEY_FILE, 'rb') as f:
-            return f.read()
+    key = os.environ.get(KEY_ENV)
+    if key:
+        return key.encode()
     else:
-        key = Fernet.generate_key()
-        with open(KEY_FILE, 'wb') as f:
-            f.write(key)
-        return key
+        # generate a new key and store it in the environment (not persistent)
+        # better to set it once manually in Railway variables
+        new_key = Fernet.generate_key()
+        print("WARNING: No ENCRYPTION_KEY env var set, using temporary key")
+        return new_key
 
 KEY = load_or_generate_key()
 cipher = Fernet(KEY)
-
-def encrypt_name(name: str) -> str:
-    return cipher.encrypt(name.encode()).decode()
-
-def decrypt_name(encrypted_name: str) -> str:
-    return cipher.decrypt(encrypted_name.encode()).decode()
