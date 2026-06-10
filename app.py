0d231752f79eb4f9cd97bc71303caf1a82b0ef28
Fartih-math <farisfatihmtk@gmail.com>
2026-06-10 16:52:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from datetime import datetime, timedelta
 import secrets
 import os
 
-application = Flask(__name__)
+app = Flask(__name__)
+
 database_url = os.environ.get('DATABASE_URL')
 if database_url and database_url.startswith('postgres://'):
     database_url = database_url.replace('postgres://', 'postgresql://', 1)
@@ -528,4 +529,4 @@ def inject_utils():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    application.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=port)
