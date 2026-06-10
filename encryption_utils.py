40483d71642c33af68130eb27f54ab2682474565
Fartih-math <farisfatihmtk@gmail.com>
2026-06-10 16:40:14 +0700

    Update encryption_utils.py

--- encryption_utils.py
+++ encryption_utils.py
@@ -5,15 +5,12 @@ KEY_FILE = 'secret.key'
 KEY_ENV = 'ENCRYPTION_KEY'
 
 def load_key():
-    # Try to get key from environment variable first
     key = os.environ.get(KEY_ENV)
     if key:
         return key.encode()
-    # Otherwise, try to read from file
     if os.path.exists(KEY_FILE):
         with open(KEY_FILE, 'rb') as f:
             return f.read()
-    # If no key exists, generate a new one and save to file (local only)
     new_key = Fernet.generate_key()
     with open(KEY_FILE, 'wb') as f:
         f.write(new_key)
