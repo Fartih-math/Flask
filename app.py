7a055210ab7e6b5c72af74b1edef1e018191918d
Fartih-math <farisfatihmtk@gmail.com>
2026-06-10 16:25:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -523,4 +523,5 @@ def inject_utils():
     return {'lang': lang, '_': _}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
